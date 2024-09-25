311aa7c225585f8c16126416aeadc4d598602830
daviddprtma <davidchristianpratama@gmail.com>
2024-09-25 10:35:25 +0700

    learning the new version of laravel 11 done

--- laravel-app/learning_laravel_11_with_docker/bootstrap/app.php
+++ laravel-app/learning_laravel_11_with_docker/bootstrap/app.php
@@ -3,17 +3,35 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
-        health: '/up',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        apiPrefix: 'api/v1/',
+        commands: __DIR__ . '/../routes/console.php',
+        channels: __DIR__ . '/../routes/channels.php',
+        health: '/status',
+        then: function () {
+            Route::prefix('admin')
+                ->name('admin.')
+                ->middleware('web')
+                ->group(base_path('routes/admin.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        // $middleware->prepend([
+        //     \App\Http\Middleware\TestMiddleware::class,
+        // ]);
+        // $middleware->appendToGroup('testGroup', [
+        //     \App\Http\Middleware\TestMiddleware::class,
+        // ]);
+
+        // $middleware->web(append: [
+        //     \App\Http\Middleware\TestMiddleware::class,
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
